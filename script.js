91440c961af65aec022379e1db759f367ed6bc26
Carl Joseph B. Diego <carljoseph.diego@tup.edu.ph>
2023-10-25 21:54:38 +0800

    Updated a file.
    Added a next and prev button.

--- script.js
+++ script.js
@@ -6,3 +6,40 @@ const clearInput = () => {
 
 const clearBtn = document.getElementById("clear-btn");
 clearBtn.addEventListener("click", clearInput);
+
+/*slides*/
+var next = document.getElementById("next");
+var prev = document.getElementById("prev");
+var main = document.getElementById("main");
+var thumbnail = document.getElementsByClassName("thumbnail");
+
+var bgimg = new Array(
+  "Assets/slide 1.png",
+  "Assets/slide 2.jpg",
+  "Assets/slide 3.png",
+  "Assets/slide 4.png",
+  "Assets/slide 5.png",
+  "Assets/slide 6.jpeg",
+  "Assets/slide 7.png",
+  "Assets/slide 8.jpg"
+);
+
+let i = 0;
+
+next.onclick = () => {
+  if (i < 7) {
+    main.style.backgroundImage = 'url("' + bgimg[i + 1] + '")';
+    thumbnail[i + 1].classList.add("active");
+    thumbnail[i].classList.remove("active");
+    i++;
+  }
+};
+
+prev.onclick = () => {
+  if (i > 0) {
+    main.style.backgroundImage = 'url("' + bgimg[i - 1] + '")';
+    thumbnail[i - 1].classList.add("active");
+    thumbnail[i].classList.remove("active");
+    i--;
+  }
+};
