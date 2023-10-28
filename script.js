31101d5f5f6aa6b9def72ab14594c1b4a881721a
vurtunegodz <carljoseph.diego@tup.edu.ph>
2023-10-28 23:46:19 +0800

    Updated files.
    Changed the slides and added a preview video

--- script.js
+++ script.js
@@ -1,45 +1,16 @@
-/*Clear Button*/
-const clearInput = () => {
-  const input = document.getElementsByTagName("input")[0];
-  input.value = "";
-};
-
-const clearBtn = document.getElementById("clear-btn");
-clearBtn.addEventListener("click", clearInput);
-
-/*slides*/
-var next = document.getElementById("next");
-var prev = document.getElementById("prev");
-var main = document.getElementById("main");
-var thumbnail = document.getElementsByClassName("thumbnail");
-
-var bgimg = new Array(
-  "Assets/slide 1.png",
-  "Assets/slide 2.png",
-  "Assets/slide 3.png",
-  "Assets/slide 4.png",
-  "Assets/slide 5.png",
-  "Assets/slide 6.png",
-  "Assets/slide 7.png",
-  "Assets/slide 8.png"
-);
-
-let i = 0;
-
-next.onclick = () => {
-  if (i < 7) {
-    main.style.backgroundImage = 'url("' + bgimg[i + 1] + '")';
-    thumbnail[i + 1].classList.add("active");
-    thumbnail[i].classList.remove("active");
-    i++;
-  }
-};
-
-prev.onclick = () => {
-  if (i > 0) {
-    main.style.backgroundImage = 'url("' + bgimg[i - 1] + '")';
-    thumbnail[i - 1].classList.add("active");
-    thumbnail[i].classList.remove("active");
-    i--;
-  }
-};
+var swiper = new Swiper(".home", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
