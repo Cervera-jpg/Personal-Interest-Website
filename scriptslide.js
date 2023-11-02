24049acbb88460548a88d9e5eb5a77f9abb73462
Namichwan <jayemmorada@gmail.com>
2023-11-02 14:31:11 +0800

    Update scriptslide

--- scriptslide.js
+++ scriptslide.js
@@ -1,3 +1,12 @@
+/*Clear Button*/
+const clearInput = () => {
+  const input = document.getElementsByTagName("input")[0];
+  input.value = "";
+};
+
+const clearBtn = document.getElementById("clear-btn");
+clearBtn.addEventListener("click", clearInput);
+
 var swiper = new Swiper(".home", {
   spaceBetween: 30,
   centeredSlides: true,
