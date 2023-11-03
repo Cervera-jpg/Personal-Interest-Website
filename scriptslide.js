1924592832e9c8fbeb9b75b740ff02c3d776351e
Namichwan <jayemmorada@gmail.com>
2023-11-03 17:00:04 +0800

    Update and removing.
    updating my branch, removing the unnecessary code and double checking my part.

--- scriptslide.js
+++ scriptslide.js
@@ -1,29 +1,3 @@
-/*Clear Button*/
-const clearInput = () => {
-  const input = document.getElementsByTagName("input")[0];
-  input.value = "";
-};
-
-const clearBtn = document.getElementById("clear-btn");
-clearBtn.addEventListener("click", clearInput);
-
-var swiper = new Swiper(".home", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
 // popular slide
 
 var swiper = new Swiper(".anime-slider", {
@@ -57,18 +31,3 @@ var swiper = new Swiper(".recommended-slider", {
   },
   loop: true,
 });
-
-// var swiper = new Swiper(".action-slider", {
-//   slidesPerView: 4,
-//   spaceBetween: 30,
-//   centeredSlides: true,
-//   autoplay: {
-//     delay: 3500,
-//     disableOnInteraction: false,
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   loop: true,
-// });
