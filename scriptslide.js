18b4fdcf4e01feb9a8aef7273374b7f337274292
vurtunegodz <carljoseph.diego@tup.edu.ph>
2023-11-04 00:14:47 +0800

    Update scriptslide.js

--- scriptslide.js
+++ scriptslide.js
@@ -14,3 +14,10 @@ var swiper = new Swiper(".home", {
     prevEl: ".swiper-button-prev",
   },
 });
+const clearInput = () => {
+  const input = document.getElementsByTagName("input")[0];
+  input.value = "";
+};
+
+const clearBtn = document.getElementById("clear-btn");
+clearBtn.addEventListener("click", clearInput);
