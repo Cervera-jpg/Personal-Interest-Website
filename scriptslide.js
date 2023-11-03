c783442bd6ddd674a00df6968bb401412b552494
Cervera-jpg <cerveraarsenio115@gmail.com>
2023-11-03 23:38:12 +0800

    Update scriptslide.js

--- scriptslide.js
+++ scriptslide.js
@@ -24,3 +24,33 @@ var swiper = new Swiper(".home", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+/*browse*/
+document.addEventListener("DOMContentLoaded", function () {
+  const genreSelect = document.getElementById("genre-select");
+  const yearSelect = document.getElementById("year-select");
+  const animeCards = document.querySelectorAll(".anime-card");
+
+  genreSelect.addEventListener("change", filterAnime);
+  yearSelect.addEventListener("change", filterAnime);
+
+  function filterAnime() {
+    const selectedGenre = genreSelect.value;
+    const selectedYear = yearSelect.value;
+
+    animeCards.forEach((card) => {
+      const genre = card
+        .querySelector("p:nth-of-type(1)")
+        .textContent.split(": ")[1];
+      const year = card
+        .querySelector("p:nth-of-type(2)")
+        .textContent.split(": ")[1];
+
+      const showCard =
+        (selectedGenre === "all" || genre === selectedGenre) &&
+        (selectedYear === "all" || year === selectedYear);
+
+      card.style.display = showCard ? "block" : "none";
+    });
+  }
+});
